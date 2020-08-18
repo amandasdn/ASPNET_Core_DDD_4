61558a75a3d03b2d502caff4b1c8c69a95d9c4f8
Amanda Nascimento <asdn.amanda@gmail.com>
2020-08-18 19:48:28 -0300

    Categories index

---
+++ Project/Project.App/Pages/Categories/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Service.Services;
+
+namespace Project.App.Pages.Categories
+{
+    public class IndexModel : ComponentBase
+    {
+        private CategoriesService _service { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
